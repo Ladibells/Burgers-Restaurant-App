456a0622816df91d445f80ce0304cb23d9281b00
ladibells <ladibell007@gmail.com>
2026-05-14 22:04:30 +0100

    designed splash screen

--- app/src/main/java/dev/ladibells/burgersrestaurantapp/feature/ui/nav/BurgerNavGraph.kt
+++ app/src/main/java/dev/ladibells/burgersrestaurantapp/feature/ui/nav/BurgerNavGraph.kt
@@ -1,10 +1,22 @@
 package dev.ladibells.burgersrestaurantapp.feature.ui.nav
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dev.ladibells.burgersrestaurantapp.feature.splash.SplashScreen
 
 @Composable
 fun BurgerNavGraph(startDestination: Screens = Screens.SplashScreen) {
 
     val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable<Screens.SplashScreen> {
+            SplashScreen()
+        }
+    }
 }
